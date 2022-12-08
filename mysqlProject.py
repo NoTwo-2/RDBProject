21539ede858acb1227a233bf2c601651638e69a4
HariBhavPatel <107136346+HariBhavPatel@users.noreply.github.com>
2022-12-08 12:07:01 -0600

    Edited tournament functions

--- mysqlProject.py
+++ mysqlProject.py
@@ -1,41 +1,61 @@
 import mysql.connector
 import os
-from secret import password
 
-# host      = localhost if the demo is running the MySQL DB on the local machine 
+
+# host      = localhost if the demo is running the MySQL DB on the local machine
 # otherwise, we will figure it out when the time comes
-# user      = username (duh) 
+# user      = username (duh)
 # password  = try to set up a secret file. we dont want our password pushed up into our git repo
 # database  = im guessing esports is correct
 mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password=password,
-  database="esports"
+    host="localhost",
+    user="root",
+    password="password",
+    database="esports"
 )
+
 
 # Create a function to show the main menu
 def show_menu():
-  os.system('clear') # Clear the screen
-  print("MySQL Command Line Interface")
-  print("----------------------------")
-  print("1. List all tables")
-  print("2. Describe a table")
-  print("3. Execute a query")
-  print("4. Add a tournament")
-  print("5. Exit")
+    os.system('clear')  # Clear the screen
+    print("MySQL Command Line Interface")
+    print("----------------------------")
+    print("1. List all tables")
+    print("2. Describe a table")
+    print("3. Execute a query")
+    print("4. Add a tournament")
+    print("5. Modify a tournament")
+    print("6. Delete a tournament")
+    print("7. Exit")
+
 
 # Create a function to list all tables in the database
 def list_tables():
-  # Get a list of all tables in the database
-  mycursor = mydb.cursor()
-  mycursor.execute("SHOW TABLES")
-  tables = mycursor.fetchall()
+    # Get a list of all tables in the database
+    mycursor = mydb.cursor()
+    mycursor.execute("SHOW TABLES")
+    tables = mycursor.fetchall()
 
-  # Print the list of tables
-  print("Tables:")
-  for table in tables:
-    print(table[0])
+    # Print the list of tables
+    print("Tables:")
+    for table in tables:
+        print(table[0])
+
+
+# Create a function to describe a table in the database
+def describe_table():
+    # Ask the user to enter the name of the table
+    table_name = input("Enter the name of the table: ")
+
+    # Get the description of the table
+    mycursor = mydb.cursor()
+    mycursor.execute("DESCRIBE " + table_name)
+    description = mycursor.fetchall()
+
+    # Print the description of the table
+    print("Table Description:")
+    for col in description:
+        print(col[0], col[1], col[2])
 
 def print_table(table):
     query = ("SELECT * FROM " + table)
@@ -47,75 +67,113 @@ def print_table(table):
     print("Tables in " + table + ":")
     for row in result:
         print(row)
-    
-# Create a function to describe a table in the database
-def describe_table():
-  # Ask the user to enter the name of the table
-  table_name = input("Enter the name of the table: ")
 
-  # Get the description of the table
-  mycursor = mydb.cursor()
-  mycursor.execute("DESCRIBE " + table_name)
-  description = mycursor.fetchall()
-
-  # Print the description of the table
-  print("Table Description:")
-  for col in description:
-    print(col[0], col[1], col[2])
 
 # Create a function to execute a query
-def execute_query():
-  # Ask the user to enter the query to execute
-  query = input("Enter the query to execute: ")
+def test_query():
+    # Ask the user to enter the query to execute
+    query = input("Enter the query to execute: ")
 
-  # Execute the query
-  mycursor = mydb.cursor()
-  mycursor.execute(query)
-  result = mycursor.fetchall()
+    # Execute the query
+    mycursor = mydb.cursor()
+    mycursor.execute(query)
+    result = mycursor.fetchall()
 
-  # Print the result of the query
-  print("Query Result:")
-  for row in result:
-    print(row)
-    
-def add_tournament():
-    print ("=== Add a tournament ===")
+    # Print the result of the query
+    print("Query Result:")
+    for row in result:
+        print(row)
+
+
+def delete_tournament():
+    print_table('tournament')
+    print("=== Delete a tournament ===")
+    tournament_id = int(input('Select tournament_id to delete '))
+    query = ("DELETE FROM tournament WHERE tournament_id = " + str(tournament_id) + ";")
+    execute_query(query,False)
+
+
+
+
+def modify_tournament():
+    #list the table
+    print_table('tournament')
+    tournament_id = int(input('Select tournament_id to modify '))
+    print("=== Modify a tournament ===")
     name = input("Enter tournament name: ")
-    state = input ("Enter tournament state: ")
+    state = input("Enter tournament state: ")
     city = input("Enter tournament city: ")
     address = input("Enter tournament street address: ")
     s_year = input("Enter tournament start year: ")
     s_month = input("Enter tournament start month: ")
     s_day = input("Enter tournament start day: ")
+    query = (
+        "UPDATE tournament SET tournament_name= %s, start_day= %s, start_month= %s, start_year= %s, street_address= %s, city= %s, state= %s "
+        "WHERE tournament_id = %s;"
+    )
+    data = (name, s_day, s_month, s_year, address, city, state, tournament_id)
+    execute_query(query,True,data)
 
-    query = "INSERT INTO tournament(tournament_name, start_day, start_month, start_year, street_address, city, state) VALUES (%s, %s, %s, %s, %s, %s)" % (name, s_day, s_month, s_year, address, city, state)
-    print(query)
-    #execute_query(query)
+
+
+
+def execute_query(query,hasdata,data = ()):
+    # Execute the query
+    mycursor = mydb.cursor()
+    if hasdata:
+        mycursor.execute(query,data)
+
+    else:
+        mycursor.execute(query)
+    mydb.commit()
+    tables = mycursor.fetchall()
+
+
+
+
+def add_tournament():
+    print("=== Add a tournament ===")
+    name = input("Enter tournament name: ")
+    state = input("Enter tournament state: ")
+    city = input("Enter tournament city: ")
+    address = input("Enter tournament street address: ")
+    s_year = input("Enter tournament start year: ")
+    s_month = input("Enter tournament start month: ")
+    s_day = input("Enter tournament start day: ")
+    query = (
+            "INSERT INTO tournament(tournament_name, start_day, start_month, start_year, street_address, city, state) "
+             "VALUES (%s, %s, %s, %s, %s, %s ,%s);"
+             )
+    data = (name, s_day, s_month, s_year, address, city, state)
+    execute_query(query,True,data)
+
 
 # Create an infinite loop to show the menu and handle user input
 while True:
-  show_menu() # Show the main menu
-  choice = int(input("Enter your choice: ")) # Ask the user to enter their choice
+    show_menu()  # Show the main menu
+    choice = int(input("Enter your choice: "))  # Ask the user to enter their choice
 
-  # Handle the user's choice
-  if choice == 1:
-    list_tables() # List all tables
-    input("Press enter to continue...") # Wait for the user to press enter
-  elif choice == 2:
-    describe_table() # Describe a table
-    input("Press enter to continue...") # Wait for the user to press enter
-  elif choice == 3:
-    execute_query() # Execute a query
-    input("Press enter to continue...") # Wait for the user to press enter
-  elif choice == 4:
-    add_tournament()
-    input("Press enter to continue...") # Wait for the user to press enter
-  elif choice == 5:
-    break # Exit the program
-
-
+    # Handle the user's choice
+    if choice == 1:
+        list_tables()  # List all tables
+        input("Press enter to continue...")  # Wait for the user to press enter
+    elif choice == 2:
+        describe_table()  # Describe a table
+        input("Press enter to continue...")  # Wait for the user to press enter
+    elif choice == 3:
+        test_query()  # Execute a query
+        input("Press enter to continue...")  # Wait for the user to press enter
+    elif choice == 4:
+        add_tournament()
+        input("Press enter to continue...")  # Wait for the user to press enter
+    elif choice == 5:
+        modify_tournament()
+        input("Press enter to continue...")  # Wait for the user to press enter
+    elif choice == 6:
+        delete_tournament()
+        input("Press enter to continue...")  # Wait for the user to press enter
+    elif choice == 7:
+        break  # Exit the program
 
 # TODO: add functions from the google doc
-# TODO: make all these functions user friendly. EX: on adding games, prompt the player for if they want to create a new roster, or use an existing one. 
-
-# https://www.w3schools.com/python/python_mysql_getstarted.asp
+# TODO: make all these functions user friendly. EX: on adding games, prompt the player for if they want to create a new roster, or use an existing one.
